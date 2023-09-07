9ac80d25b8c4e871fae97518d3a1d1786de77523
DESKTOP-7V9Q99B\AKU <akuraza6atgmail.com>
2023-09-07 11:16:51 +0500

    charAt

--- string.js
+++ string.js
@@ -1,10 +1,14 @@
-const str=`hi this is raza`
-console.log(typeof str)
-console.log(str)
-const str1= new String("hi this is raza")
-console.log(str1.charAt(1))
-console.log(typeof str1)
-const str2="hi this is raza"
-console.log(str2,"---",str2[0])
+// const str=`hi this is raza`
+// console.log(typeof str)
+// console.log(str)
+// const str1= new String("hi this is raza")
+// console.log(str1.charAt(1))
+// console.log(typeof str1)
+// const str2="hi this is raza"
+// console.log(str2,"---",str2[0])
+
+
+const newstr="hi develoepr"
+console.log(newstr.charAt(9))
 
 
