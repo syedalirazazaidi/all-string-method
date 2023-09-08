4f5ad31561b8c459d7432e99b8cf20265ec04122
DESKTOP-7V9Q99B\AKU <akuraza6atgmail.com>
2023-09-08 11:28:37 +0500

    Filter Method

--- array.js
+++ array.js
@@ -18,3 +18,14 @@ console.log("Array Method")
 
 // console.log(modifiedArr);
 // console.log(arr)
+
+// Filter Method
+
+// let arr = [3, 4, 5, 6];
+
+// let modifiedArr = arr.filter(function(element){
+//     return element <4;
+// });
+
+// console.log(modifiedArr);
+// console.log(arr)
