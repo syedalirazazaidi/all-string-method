dc6bf2bad0072c4a4b7f82184bfa10259e188b8b
DESKTOP-7V9Q99B\AKU <akuraza6atgmail.com>
2023-09-08 11:25:50 +0500

    Map Method

---
+++ array.js
@@ -0,0 +1,20 @@
+console.log("Array Method")
+
+// forEach Method
+
+// const fruits = ["apple", "orange", "cherry"];
+// const newarr=fruits.forEach((elem)=>
+// elem
+// );
+// console.log(newarr)
+
+// Map Method
+
+// let arr = [3, 4, 5, 6];
+
+// let modifiedArr = arr.map(function(element){
+//     return element *3;
+// });
+
+// console.log(modifiedArr);
+// console.log(arr)
